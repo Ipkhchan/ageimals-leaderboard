6fd833f7cd86cc55686d0eef445d70b8576cfb80
bkzhang <brian.zhang@shopify.com>
2019-04-03 14:27:19 -0400

    base graphql

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const schema = require('./graphqlSchema');
+
 const Koa = require("koa");
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
 const app = new Koa();
-app.use(async ctx => {
-  ctx.body = "Hello World";
-});
+
+app.use(mount('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+})));
+
 app.listen(3000);
+console.log('Running a GraphQL API server at localhost:3000/graphql');
