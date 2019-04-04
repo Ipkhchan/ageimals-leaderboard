53079a97090e2639b68d98b6b39aec6f33e19b66
Nashua Luk <nashua.luk@shopify.com>
2019-04-04 17:26:55 -0400

    added post losers

--- graphql/root.js
+++ graphql/root.js
@@ -3,13 +3,15 @@ const getBottomOfLeaderboard = require("./getBottomOfLeaderboard.js");
 const getTopWinStreaks = require("./getTopWinStreaks.js");
 const getTopLoseStreaks = require("./getTopLoseStreaks.js");
 const postWinners = require("./postWinners.js");
+const postLosers = require("./postLosers.js");
 
 const root = {
   mostWins: ({ numUsers }) => getTopOfLeaderboard(numUsers),
   mostLosses: ({ numUsers }) => getBottomOfLeaderboard(numUsers),
   highestWinStreaks: ({ numUsers }) => getTopWinStreaks(numUsers),
   highestLoseStreaks: ({ numUsers }) => getTopLoseStreaks(numUsers),
-  winners: ({ userHandles }) => postWinners(userHandles)
+  winners: ({ userHandles }) => postWinners(userHandles),
+  losers: ({ userHandles }) => postLosers(userHandles)
 };
 
 module.exports = root;
