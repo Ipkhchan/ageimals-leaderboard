2627276b19a99496b7cc73948a5043d4150f3b1f
Joey Zhou <joey.zhou@shopify.com>
2019-04-05 17:16:25 -0400

    Fixing postLosers Test

--- middleware/crud-functions/crud-function-tests/postLosersTest.js
+++ middleware/crud-functions/crud-function-tests/postLosersTest.js
@@ -1,28 +1,33 @@
 const db = require('../../../firebase.js');
 const postLosers = require('../postLosers.js');
-const should = require('chai').should();
+const assert = require('chai').assert;
 
 describe('postLosers', function() {
   it('should return true for adding user to db', async function() {
-    response = await postLosers(['nash']);
-    revertLossData(['nash']);
-    response.should.equal(true);
+    const winners = ["nash"];
+    const response = await postLosers(winners);
+    assert.equal(response, winners);
+    revertLossData(winners);
   });
 });
 
 function revertLossData(userHandles) {
-  var ref = db.collection('users');
-  const document = ref.doc(userHandles[0]);
-  document
-    .update({
+  const ref = db.collection("users");
+  const promises = userHandles.map(async user => {
+    const document = ref.doc(user);
+    return document.update({
       losses: admin.firestore.FieldValue.increment(-1),
       lossStreak: admin.firestore.FieldValue.increment(-1),
       winStreak: 0,
-    })
-    .then(function() {
-      console.log('successfully updated!');
-    })
-    .catch(function(error) {
-      console.error('Error writing user: ', error);
     });
+  });
+
+  Promise.all(promises)
+    .then((_result) => {
+      console.log("User data successfully reverted!");
+    })
+    .catch((error) => {
+      console.log("Error reverting user data: " + error.message + ".");
+    });
+  return;
 }
